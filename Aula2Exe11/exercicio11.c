184ca768574bd996e7441e45428334f05c6a7c99
Matheus Costa <matheuscostap@gmail.com>
2019-04-07 12:50:53 -0300

    Exercicio 11

---
+++ Aula2Exe11/exercicio11.c
@@ -0,0 +1,71 @@
+//
+//  main.c
+//  Aula2Exe11
+//
+//  Created by Matheus Prates da Costa on 07/04/2019.
+//  Copyright © 2019 Matheus Prates da Costa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int formaTriangulo(int a, int b, int c);
+int ehIsosceles(int a, int b, int c);
+int ehEquilatero(int a, int b, int c);
+int ehEscaleno(int a, int b, int c);
+
+int main(int argc, const char * argv[]) {
+    int a;
+    int b;
+    int c;
+
+    printf("Digite os valores dos 3 lados do triangulo: \n");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+
+    if (formaTriangulo(a,b,c)){
+      if(ehEscaleno(a,b,c)){
+        printf("Os valores formam um triangulo escaleno");
+      }else if(ehEquilatero(a,b,c)){
+        printf("Os valores formam um triangulo equilatero");
+      }else if (ehIsosceles(a,b,c)) {
+        printf("Os valores formam um triangulo isosceles");
+      }
+    }else{
+      printf("Os valores nao formam um triangulo");
+    }
+
+    return 0;
+}
+
+int formaTriangulo(int a, int b, int c){
+  if ((a < b + c) && (b < a + c) && (c < a + b)) {
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+int ehIsosceles(int a, int b, int c){
+  if ((a==b) || (a==c) || (b==c)) {
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+int ehEquilatero(int a, int b, int c){
+  if ((a==b) && (b==c)) {
+    return 1;
+  }else{
+    return 0;
+  }
+}
+
+int ehEscaleno(int a, int b, int c){
+  if ((a!=b) && (a!=c) && (b!=c)) {
+    return 1;
+  }else{
+    return 0;
+  }
+}
